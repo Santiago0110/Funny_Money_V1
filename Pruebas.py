e92c322d6e914a37f6ad78636707a72521b7250b
Santiago Lasso <santiago97.sl@gmail.com>
2022-08-26 13:35:01 -0300

    En esta versión se puede seleccionar una categoría al random, e igualmente una opción al random de la categoría seleccionada - Archivo Pruebas

--- Pruebas.py
+++ Pruebas.py
@@ -7,12 +7,22 @@ Created on Fri Aug 26 11:22:56 2022
 
 import random 
 
-categorias = {
+cats = {
     'Ganar Dinero': ['A1','A2','A3','A4','A5','A6','A7','A8'],
     'Hacer Compras': ['B1','B2','B3','B4','B5','B6','B7','B8'],
     'Recompensas': ['C1','C2','C3','C4','C5','C6','C7','C8']     
 }
 
+# Seleccionar el nombre de una categoría al random
+cat_chosen = random.choice(list(cats.keys()))
+print(cat_chosen)
+
+# Seleccionar una opción dentro de una categoría
+opt_chosen = random.choice(cats[cat_chosen][:])
+print(opt_chosen)
+
+
+"""
 for key in categorias.keys():
     print(key)
 
@@ -25,4 +35,4 @@ print('\n')
     
 for key,value in categorias.items():
     print(key,'-',value)
-    
+"""
