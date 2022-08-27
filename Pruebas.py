4c5984ae1da5f952ea73337af917e32bb54c3b67
Santiago Lasso <santiago97.sl@gmail.com>
2022-08-26 21:01:37 -0300

    Haciendo pruebas de loop en la ronda

--- Pruebas.py
+++ Pruebas.py
@@ -15,32 +15,46 @@ cats = {
 }
 
 cats2 = copy.deepcopy(cats)
+cont = 0
+cat_chosen = ''
+cat_updt = []
 
 def removeOption(d,key,opt):
     r = dict(d)
     del r[key][opt]
     return r
 
-# Seleccionar el nombre de una categoría al random
-cat_chosen = random.choice(list(cats.keys()))
-print(cat_chosen)
 
-# Seleccionar una opción dentro de una categoría
-opt_chosen = random.choice(cats[cat_chosen])
-print(opt_chosen)
-
-# Encuentra el índice de la categoría elegida
-indexCat = list(cats.keys()).index(cat_chosen)
-print('El index de ' + cat_chosen + ' es = ' + str(indexCat))
-
-# Encuentra el índice de la opción elegida dentro de la categoría
-indexOpt = cats[cat_chosen].index(opt_chosen)
-print('El index de ' + opt_chosen + ' es = ' + str(indexOpt))
-
-#del cats_copy[cat_chosen][indexOpt]
-cats2 = removeOption(cats2,cat_chosen,indexOpt)
-
-#remove = {k:v for k,v in cats.items() if v != opt_chosen}
-
-#xxxxxxxxxx
+while cont < 3:
+    
+    if cat_chosen:
+        
+        # Seleccionar el nombre de una categoría al random
+        cat_chosen = random.choice(list(cats2.keys()))
+        print(cat_chosen)
+        
+        # Seleccionar una opción dentro de una categoría
+        opt_chosen = random.choice(cats2[cat_chosen])
+        print(opt_chosen)
+        
+        #cat_updt = {k for k,v in cats2.items() if k != cat_chosen}
+        
+        # Encuentra el índice de la categoría elegida
+        indexCat = list(cats.keys()).index(cat_chosen)
+        print('El index de ' + cat_chosen + ' es = ' + str(indexCat))
+        
+        # Encuentra el índice de la opción elegida dentro de la categoría
+        indexOpt = cats[cat_chosen].index(opt_chosen)
+        print('El index de ' + opt_chosen + ' es = ' + str(indexOpt))
+        
+        # Elimina la opción de la lista
+        cats2 = removeOption(cats2,cat_chosen,indexOpt)
+        
+        cat_updt.append()
+        
+        cont += 1
+        
+    print(cats2)
+    
+    #remove = {k:v for k,v in cats.items() if v != opt_chosen}
 
