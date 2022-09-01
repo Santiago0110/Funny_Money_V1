ee9adcc5f3d2f084eccac98e21ab9108ea10235d
Santiago Lasso <santiago97.sl@gmail.com>
2022-09-01 15:40:28 -0300

    El código de PRUEBAS actual, permite seleccionar el turno de un jugador, seleccionar un categoría y una opción; al mismo tiempo elimina la categoría seleccionada y remueve la categoría elegida de una lista preliminar, permitiendo que no se repitan las categorías para un jugador

--- Pruebas.py
+++ Pruebas.py
@@ -1,11 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 26 11:22:56 2022
+"""------------------------------------------------------
+LIBRERÍAS IMPORTADAS
+------------------------------------------------------"""
+import random
+import copy
+from collections import deque
 
-@author: usuario
-"""
-
-import random 
+"""------------------------------------------------------
+DECLARACIÓN DE VARIABLES
+------------------------------------------------------"""
+cont_turno = 0
+turno = deque([1,2])
 
 cats = {
     'Ganar Dinero': ['A1','A2','A3','A4','A5','A6','A7','A8'],
@@ -13,28 +17,84 @@ cats = {
     'Recompensas': ['C1','C2','C3','C4','C5','C6','C7','C8']     
 }
 
-cats_copy = cats.copy()
+cats2 = copy.deepcopy(cats)
+
+"""------------------------------------------------------
+DECLARACÍÓN DE FUNCIONES
+------------------------------------------------------"""
+def select_cat(opciones):
+    opciones = opciones
+    while True:
+        r = random.choice(opciones)
+        opciones.remove(r)
+        yield r
 
 def removeOption(d,key,opt):
     r = dict(d)
     del r[key][opt]
     return r
 
-# Seleccionar el nombre de una categoría al random
-cat_chosen = random.choice(list(cats.keys()))
-print(cat_chosen)
+def rotar_turno():
+	turno.rotate()
+	return turno[0]
 
-# Seleccionar una opción dentro de una categoría
-opt_chosen = random.choice(cats[cat_chosen])
-print(opt_chosen)
+"""------------------------------------------------------
+PROGRAMA PRINCIPAL
+------------------------------------------------------"""
+if __name__ == '__main__':
+    
+    # Indica a qué jugador le corresponde el turno
+    jugador = rotar_turno()
+    
+    if jugador == 2:    
+        categorias = list(cats2.keys())
+        
+        while cont_turno < 3:
+       
+            select = select_cat(categorias)
+            cat_selected = next(select)
+            
+            opt_selected = random.choice(cats[cat_selected])
+            index_opt = cats[cat_selected].index(opt_selected)
+            
+            removeOption(cats2,cat_selected,index_opt)
+            cont_turno+=1
 
-# Encuentra el índice de la categoría elegida
-indexCat = list(cats.keys()).index(cat_chosen)
-print('El index de ' + cat_chosen + ' es = ' + str(indexCat))
 
-# Encuentra el índice de la opción elegida dentro de la categoría
-indexOpt = cats[cat_chosen].index(opt_chosen)
-print('El index de ' + opt_chosen + ' es = ' + str(indexOpt))
+"""
+Código que elige una opción de una lista al random
+y la guarda en una variable SIN REPETIR
+"""
+# import random
 
-#del cats_copy[cat_chosen][indexOpt]
-cats_act = removeOption(cats_copy,cat_chosen,indexOpt)
+# def aleatorio(opciones):
+#     opciones = opciones
+#     while True:
+#         r = random.choice(opciones)
+#         opciones.remove(r)
+#         yield r
+
+# a = ['Ganar Dinero','Hacer Compras','Recompensas']
+# gen = aleatorio(a)
+
+# L1 = next(gen)
+# L2 = next(gen)
+# L3 = next(gen)
+
+"""
+Código para rotar turnos de jugador
+"""
+
+# from collections import deque
+ 
+# turno = deque(["1", "2"])
+# cont = 0
+
+# def rotar_turno():
+# 	turno.rotate()
+# 	return turno[0]
+
+# while cont <= 10:
+#     jugador = rotar_turno()
+#     print(jugador)
+#     cont+=1
