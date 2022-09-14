ce64bf4cc491ec7b4edc364f3497c85d799cf99f
Santiago Lasso <santiago97.sl@gmail.com>
2022-09-13 22:47:06 -0300

    Se comprobó la tira led, junto con los leds de jugadores y categorías

--- Control_De_Hardware/Prueba_LEDS_TiraLED/Prueba_LEDS_TiraLED.ino
+++ Control_De_Hardware/Prueba_LEDS_TiraLED/Prueba_LEDS_TiraLED.ino
@@ -2,6 +2,8 @@
 int Led_J1 = 5,Led_J2 = 9; 
 // Leds de categorías
 int Led_GD = 6, Led_HC = 7, Led_RE = 8;
+// Tira LED
+int Tira_LED = 2;
 
 void setup()
 {
@@ -10,6 +12,7 @@ void setup()
   pinMode(Led_GD, OUTPUT);
   pinMode(Led_HC, OUTPUT);
   pinMode(Led_RE, OUTPUT);
+  pinMode(Tira_LED, OUTPUT);
 }
 
 void loop()
@@ -19,11 +22,13 @@ void loop()
   digitalWrite(Led_GD,HIGH);
   digitalWrite(Led_HC,HIGH);
   digitalWrite(Led_RE,HIGH);
+  digitalWrite(Tira_LED,HIGH);
   delay(200);
   digitalWrite(Led_J1,LOW);
   digitalWrite(Led_J2,LOW);
   digitalWrite(Led_GD,LOW);
   digitalWrite(Led_HC,LOW);
   digitalWrite(Led_RE,LOW);
+  digitalWrite(Tira_LED,LOW);
   delay(200); 
 }
