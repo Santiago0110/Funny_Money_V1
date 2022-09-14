06b8342465b5bf63240249ca7f7d7f9bf4b5b4ec
Santiago Lasso <santiago97.sl@gmail.com>
2022-09-13 22:34:17 -0300

    Se comprobaron los leds de las categorías, y al mismo tiempo los leds de jugadores

--- Control_De_Hardware/Prueba_LEDS_TiraLED/Prueba_LEDS_TiraLED.ino
+++ Control_De_Hardware/Prueba_LEDS_TiraLED/Prueba_LEDS_TiraLED.ino
@@ -1,18 +1,29 @@
 // Leds de jugadores
 int Led_J1 = 5,Led_J2 = 9; 
+// Leds de categorías
+int Led_GD = 6, Led_HC = 7, Led_RE = 8;
 
 void setup()
 {
   pinMode(Led_J1, OUTPUT);
   pinMode(Led_J2, OUTPUT);
+  pinMode(Led_GD, OUTPUT);
+  pinMode(Led_HC, OUTPUT);
+  pinMode(Led_RE, OUTPUT);
 }
 
 void loop()
 {
   digitalWrite(Led_J1,HIGH);
   digitalWrite(Led_J2,HIGH);
+  digitalWrite(Led_GD,HIGH);
+  digitalWrite(Led_HC,HIGH);
+  digitalWrite(Led_RE,HIGH);
   delay(200);
   digitalWrite(Led_J1,LOW);
   digitalWrite(Led_J2,LOW);
+  digitalWrite(Led_GD,LOW);
+  digitalWrite(Led_HC,LOW);
+  digitalWrite(Led_RE,LOW);
   delay(200); 
 }
