5cc65183bd4f22b434981b073526a17278e45b1c
Santiago Lasso <santiago97.sl@gmail.com>
2022-09-13 23:06:06 -0300

    Agregué una función de fade a los leds de jugadores

--- Control_De_Hardware/Prueba_LEDS_TiraLED/Prueba_LEDS_TiraLED.ino
+++ Control_De_Hardware/Prueba_LEDS_TiraLED/Prueba_LEDS_TiraLED.ino
@@ -5,6 +5,9 @@ int Led_GD = 6, Led_HC = 7, Led_RE = 8;
 // Tira LED
 int Tira_LED = 2;
 
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
 void setup()
 {
   pinMode(Led_J1, OUTPUT);
@@ -15,20 +18,33 @@ void setup()
   pinMode(Tira_LED, OUTPUT);
 }
 
+void fade()
+{
+  analogWrite(Led_J1, brightness);
+  analogWrite(Led_J2, brightness);
+  brightness = brightness + fadeAmount;
+  if (brightness <= 0 || brightness >= 255)
+  {
+    fadeAmount = -fadeAmount;
+  }
+  delay(30);
+}
+
 void loop()
 {
-  digitalWrite(Led_J1,HIGH);
-  digitalWrite(Led_J2,HIGH);
+//  digitalWrite(Led_J1,HIGH);
+//  digitalWrite(Led_J2,HIGH);
+  fade();
   digitalWrite(Led_GD,HIGH);
   digitalWrite(Led_HC,HIGH);
   digitalWrite(Led_RE,HIGH);
   digitalWrite(Tira_LED,HIGH);
-  delay(200);
-  digitalWrite(Led_J1,LOW);
-  digitalWrite(Led_J2,LOW);
-  digitalWrite(Led_GD,LOW);
-  digitalWrite(Led_HC,LOW);
-  digitalWrite(Led_RE,LOW);
-  digitalWrite(Tira_LED,LOW);
-  delay(200); 
+//  delay(200);
+//  digitalWrite(Led_J1,LOW);
+//  digitalWrite(Led_J2,LOW);
+//  digitalWrite(Led_GD,LOW);
+//  digitalWrite(Led_HC,LOW);
+//  digitalWrite(Led_RE,LOW);
+//  digitalWrite(Tira_LED,LOW);
+//  delay(200); 
 }
